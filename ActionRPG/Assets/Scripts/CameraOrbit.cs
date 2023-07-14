d0e7a57026ba159c40a602bd143b88c12bb0f229
Jedi-Daddy <av.zhuravlev90@gmail.com>
2023-07-14 13:50:59 +0200

    camera Orbit setup

--- ActionRPG/Assets/Scripts/CameraOrbit.cs
+++ ActionRPG/Assets/Scripts/CameraOrbit.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class CameraOrbit : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float lookSensitivity;
+    public float minXlook;
+    public float maxXlook;
+    public Transform camAnchor;
+
+    public bool invertXrotation;
+
+    private float curXRot;
+
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
-
-    // Update is called once per frame
-    void Update()
+    //called at the end of each frame
+    void LateUpdate()
     {
-        
+        //get the mouse X and Y inputs
+        float x = Input.GetAxis("Mouse X");
+        float y = Input.GetAxis("Mouse Y");
+
+        //rotate the player horizontally
+        transform.eulerAngles += Vector3.up * x * lookSensitivity;
+
+        if (invertXrotation )
+            curXRot += y * lookSensitivity;
+        else
+            curXRot -= y * lookSensitivity;
+
+        curXRot = Mathf.Clamp(curXRot, minXlook, maxXlook);
+
+        Vector3 clampedAngle = camAnchor.eulerAngles;
+        clampedAngle.x = curXRot;
+
+        camAnchor.eulerAngles = clampedAngle;
     }
 }
