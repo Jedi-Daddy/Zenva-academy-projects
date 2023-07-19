a90c1c51b2096b28f878b90cc23f2ac41a86a066
Jedi-Daddy <av.zhuravlev90@gmail.com>
2023-07-19 13:29:51 +0200

    add Moving the Enemy

--- ActionRPG/Assets/Scripts/Enemy.cs
+++ ActionRPG/Assets/Scripts/Enemy.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Player player;
+    public float attacDistance;
+    public int damage;
+    public int health;
 
-    // Update is called once per frame
+    private bool isAttacking;
+    private bool isDead;
+
+    public NavMeshAgent agent;
+
     void Update()
     {
-        
+        if(isDead)
+            return;
+
+        if(Vector3.Distance(transform.position, player.transform.position) <= attacDistance)
+        {
+            agent.isStopped = true;
+            // attack code
+        }
+        else
+        {
+            agent.isStopped = false;
+            agent.SetDestination(player.transform.position);
+        }
     }
 }
