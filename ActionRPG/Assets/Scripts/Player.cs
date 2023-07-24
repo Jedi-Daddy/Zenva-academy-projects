035e33d1da2bf7fa1be1cd288f2322314cbddad9
Jedi-Daddy <av.zhuravlev90@gmail.com>
2023-07-24 15:48:27 +0200

    add code for Player animations

--- ActionRPG/Assets/Scripts/Player.cs
+++ ActionRPG/Assets/Scripts/Player.cs
@@ -16,6 +16,7 @@ public class Player : MonoBehaviour
     private bool isAttacking;
 
     public Rigidbody rig;
+    public Animator anim;
     
 
     void Update()
@@ -27,6 +28,9 @@ public class Player : MonoBehaviour
 
         if(Input.GetMouseButtonDown(0) && !isAttacking)
             Attack();
+
+        if(!isAttacking)
+            UpdateAnimator();
     }
 
 
@@ -40,6 +44,25 @@ public class Player : MonoBehaviour
         dir.y = rig.velocity.y;
 
         rig.velocity = dir;
+    }
+
+    void UpdateAnimator()
+    {
+        anim.SetBool("MovingForwards", false);
+        anim.SetBool("MovingBackwards", false);
+        anim.SetBool("MovingLeft", false);
+        anim.SetBool("MovingRight", false);
+
+        Vector3 localVel = transform.InverseTransformDirection(rig.velocity);
+
+        if (localVel.z > 0.1f)
+            anim.SetBool("MovingForwards", true);
+        else if (localVel.z < -0.1f)
+            anim.SetBool("MovingBackwards", true);
+        else if (localVel.x < -0.1f)
+            anim.SetBool("MovingLeft", true);
+        else if (localVel.x > 0.1f)
+            anim.SetBool("MovingRight", true);
     }
 
     void Jump()
@@ -78,6 +101,7 @@ public class Player : MonoBehaviour
     void Attack()
     {
         isAttacking = true;
+        anim.SetTrigger("Attack");
         
         Invoke("TryDamage", 0.7f);
         Invoke("DisableIsAttacking", 1.5f);
