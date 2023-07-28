0f85d48cb387211baf3fa537eb2909f0a3fc1fc5
Jedi-Daddy <av.zhuravlev90@gmail.com>
2023-07-28 13:36:32 +0200

    add carControllerScript

--- ArcadeKartingGame/Assets/Scripts/CarController.cs
+++ ArcadeKartingGame/Assets/Scripts/CarController.cs
@@ -1,18 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CarController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float acceleration;
+    public float turnSpeed;
+
+    public Transform carModel;
+    private Vector3 startModelOffset;
+
+    public float groundCheckRate;
+    private float lastGroundCheckTime;
+
+    private float curvYRot;
+
+    private bool accelerateInput;
+    private float turnInput;
+
+    public Rigidbody rig;
+
     void Start()
     {
-        
+        startModelOffset = carModel.transform.localPosition;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        curvYRot += turnInput * turnSpeed * Time.deltaTime;
+
+        //Debug.Log("Accelerating: " + accelerateInput + ", Turn: " + turnInput);
+        carModel.position = transform.position + startModelOffset;
+        carModel.eulerAngles = new Vector3(0,curvYRot,0);
+    }
+
+    // 60 times per seconds
+    void FixedUpdate()
+    {
+        if(accelerateInput == true)
+        {
+            rig.AddForce(carModel.forward * acceleration, ForceMode.Acceleration);
+        }
+    }
+
+    public void OnAccelerateInput(InputAction.CallbackContext context)
+    {
+        if(context.phase == InputActionPhase.Performed)
+            accelerateInput = true;
+        else
+            accelerateInput = false;
+    }
+
+    public void OnTurnInput (InputAction.CallbackContext context)
+    {
+        turnInput = context.ReadValue<float>();
     }
 }
