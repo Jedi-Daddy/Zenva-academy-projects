5ea32c7d49d60eb10fb4756c146036db2b373b85
Jedi-Daddy <av.zhuravlev90@gmail.com>
2023-07-28 15:26:48 +0200

    add calculating race position + fix second road colladers

--- ArcadeKartingGame/Assets/Scripts/GameManager.cs
+++ ArcadeKartingGame/Assets/Scripts/GameManager.cs
@@ -6,10 +6,44 @@ public class GameManager : MonoBehaviour
 {
     public List<CarController> cars = new List<CarController> ();
 
+    public float positionUpdateRate = 0.05f;
+    private float lastPositionUpdateTime;
+
     public static GameManager Instance;
 
     void Awake()
     {
         Instance = this;
+    }
+
+    void Update()
+    {
+        if(Time.time - lastPositionUpdateTime > positionUpdateRate)
+        {
+            lastPositionUpdateTime = Time.time;
+            UpdateCarRacePositions();
+        }
+    }
+
+    void UpdateCarRacePositions()
+    {
+        cars.Sort(SortPosition);
+
+        for(int x=0; x<cars.Count; x++)
+        {
+            cars[x].racePosition = cars.Count - x;
+        }
+    }
+
+    int SortPosition (CarController a, CarController b)
+    {
+        if(a.zonesPassed > b.zonesPassed)
+            return 1;
+        else if(b.zonesPassed > a.zonesPassed)
+            return -1;
+        float aDist = Vector3.Distance(a.transform.position, a.curTrackZone.transform.position);
+        float bDist = Vector3.Distance(b.transform.position, b.curTrackZone.transform.position);
+
+        return aDist > bDist ? 1 : -1;
     }
 }
