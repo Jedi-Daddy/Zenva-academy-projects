62b3863bcaf0f7c392a6b3f2eb2a990a2cc96d05
Jedi-Daddy <av.zhuravlev90@gmail.com>
2023-07-30 22:09:16 +0200

    add playercontroller script and write moving functions

--- SurvivalAcademy/Assets/Scripts/Player/PlayerController.cs
+++ SurvivalAcademy/Assets/Scripts/Player/PlayerController.cs
@@ -5,6 +5,11 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement")]
+    public float moveSpeed;
+    private Vector2 curMovementInput;
+
+    [Header("Look")]
     public Transform cameraContainer;
     public float minXLook;
     public float maxXLook;
@@ -13,9 +18,24 @@ public class PlayerController : MonoBehaviour
 
     private Vector2 mouseDelta;
 
+    // components
+    private Rigidbody rig;
+
+    void Awake()
+    {
+        // get our components
+        rig = GetComponent<Rigidbody>();
+    }
+
     void Start()
     {
+        // lock the cursor at the start of the game
         Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void FixedUpdate()
+    {
+        Move();
     }
 
     void LateUpdate()
@@ -23,17 +43,39 @@ public class PlayerController : MonoBehaviour
         CameraLook();
     }
 
+    void Move()
+    {
+        Vector3 dir = transform.forward * curMovementInput.y + transform.right * curMovementInput.x;
+        dir *= moveSpeed;
+        dir.y = rig.velocity.y;
+
+        rig.velocity = dir;
+    }
+
     void CameraLook()
     {
+        // rotate the player camera up and down
         camCurXRot += mouseDelta.y * lookSensitivity;
         camCurXRot = Mathf.Clamp(camCurXRot, minXLook, maxXLook);
         cameraContainer.localEulerAngles = new Vector3(-camCurXRot,0,0);
-
+        // rotate the player camera left and right
         transform.eulerAngles += new Vector3(0, mouseDelta.x * lookSensitivity, 0);
     }
-
+    // called when we move our mouse - manaaged by the Input System
     public void OnLookInput (InputAction.CallbackContext context)
     {
         mouseDelta = context.ReadValue<Vector2>();
+    }
+
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        if(context.phase == InputActionPhase.Performed)
+        {
+            curMovementInput = context.ReadValue<Vector2>();
+        }
+        else if(context.phase == InputActionPhase.Canceled)
+        {
+            curMovementInput = Vector2.zero;
+        }
     }
 }
