575d561d03ae1b801bc84cb7c8fe7163ec9a3356
Jedi-Daddy <av.zhuravlev90@gmail.com>
2023-08-01 17:28:59 +0200

    add jumping for player

--- SurvivalAcademy/Assets/Scripts/Player/PlayerController.cs
+++ SurvivalAcademy/Assets/Scripts/Player/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     [Header("Movement")]
     public float moveSpeed;
     private Vector2 curMovementInput;
+    public float jumpForce;
+    public LayerMask groundLayerMask;
 
     [Header("Look")]
     public Transform cameraContainer;
@@ -57,25 +59,71 @@ public class PlayerController : MonoBehaviour
         // rotate the player camera up and down
         camCurXRot += mouseDelta.y * lookSensitivity;
         camCurXRot = Mathf.Clamp(camCurXRot, minXLook, maxXLook);
-        cameraContainer.localEulerAngles = new Vector3(-camCurXRot,0,0);
+        cameraContainer.localEulerAngles = new Vector3(-camCurXRot, 0, 0);
         // rotate the player camera left and right
         transform.eulerAngles += new Vector3(0, mouseDelta.x * lookSensitivity, 0);
     }
     // called when we move our mouse - manaaged by the Input System
-    public void OnLookInput (InputAction.CallbackContext context)
+    public void OnLookInput(InputAction.CallbackContext context)
     {
         mouseDelta = context.ReadValue<Vector2>();
     }
 
     public void OnMoveInput(InputAction.CallbackContext context)
     {
-        if(context.phase == InputActionPhase.Performed)
+        if (context.phase == InputActionPhase.Performed)
         {
             curMovementInput = context.ReadValue<Vector2>();
         }
-        else if(context.phase == InputActionPhase.Canceled)
+        else if (context.phase == InputActionPhase.Canceled)
         {
             curMovementInput = Vector2.zero;
         }
     }
+    // called when we press down on the spacebar - managed by the Input System
+    public void OnJumpInput(InputAction.CallbackContext context)
+    {
+        // is this the first frame we're pressing the button?
+        if (context.phase == InputActionPhase.Started)
+        {
+            // are we standing on the ground?
+            if (IsGrounded())
+            {
+                // add force updwards
+                rig.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            }
+        }
+    }
+
+    bool IsGrounded()
+    {
+        Ray[] rays = new Ray[4]
+        {
+            new Ray(transform.position + (transform.forward * 0.2f) + (Vector3.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (-transform.forward * 0.2f) + (Vector3.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (transform.right * 0.2f) + (Vector3.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (-transform.right * 0.2f) + (Vector3.up * 0.01f), Vector3.down)
+        };
+
+        for(int i=0; i< rays.Length; i++)
+        {
+            if (Physics.Raycast(rays[i], 0.1f, groundLayerMask))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawRay(transform.position + (transform.forward * 0.2f), Vector3.down);
+        Gizmos.DrawRay(transform.position + (-transform.forward * 0.2f), Vector3.down);
+        Gizmos.DrawRay(transform.position + (transform.right * 0.2f), Vector3.down);
+        Gizmos.DrawRay(transform.position + (-transform.right * 0.2f), Vector3.down);
+    }
+
 }
