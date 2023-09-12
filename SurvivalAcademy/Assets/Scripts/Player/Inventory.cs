87ba96e6f0e837cb70694e809a9815ba6a297f8e
Jedi-Daddy <av.zhuravlev90@gmail.com>
2023-09-12 16:25:30 +0200

    add adding items script

--- SurvivalAcademy/Assets/Scripts/Player/Inventory.cs
+++ SurvivalAcademy/Assets/Scripts/Player/Inventory.cs
@@ -57,6 +57,109 @@ public class Inventory : MonoBehaviour
             uiSlots[x].Clear();
         }
     }
+    public void Toggle()
+    {
+
+    }
+
+    public bool IsOpen()
+    {
+        return inventoryWindow.activeInHierarchy;
+    }
+
+    public void AddItem(ItemData item)
+    {
+        if (item.canStack)
+        {
+            ItemSlot slotToStackTo = GetItemStack(item);
+
+            if (slotToStackTo != null)
+            {
+                slotToStackTo.quantity++;
+                UpdateUI();
+                return;
+            }
+        }
+        ItemSlot emptySlot = GetEmptySlot();
+
+        if (emptySlot != null)
+        {
+            emptySlot.item = item;
+            emptySlot.quantity = 1;
+            UpdateUI();
+            return;
+        }
+
+        ThrowItem(item);
+    }
+
+    void ThrowItem(ItemSlot item)
+    {
+
+    }
+
+    void UpdateUI()
+    {
+
+    }
+
+    ItemSlot GetItemStack(ItemData item)
+    {
+        return null;
+    }
+
+    ItemSlot GetEmptySlot() 
+    { 
+        return null; 
+    }
+
+    public void SelectItem (int index)
+    {
+
+    }
+
+    void ClearSelectedItemWindow()
+    {
+
+    }
+    public void OnUseButton()
+    {
+
+    }
+
+    public void OnEquipButton()
+    {
+
+    }
+    void UnEquip(int index)
+    {
+
+    }
+
+    public void OnUnEquipButton()
+    {
+
+    }
+
+    public void OnDropButton()
+    {
+
+    }
+
+    void RemoveSelectedItem()
+    {
+
+    }
+
+    public void RemoveItem(ItemData item)
+    {
+
+    }
+
+    public bool HasItems(ItemData item, int quantity)
+    {
+        return false;
+    }
 }
 
 public class ItemSlot
