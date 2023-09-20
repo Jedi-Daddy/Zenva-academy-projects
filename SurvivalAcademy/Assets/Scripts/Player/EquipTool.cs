15712bcd369576c7234636c7ab7dfeede2ee25ee
Jedi-Daddy <av.zhuravlev90@gmail.com>
2023-09-20 14:35:09 +0200

    add equip tool script

--- SurvivalAcademy/Assets/Scripts/Player/EquipTool.cs
+++ SurvivalAcademy/Assets/Scripts/Player/EquipTool.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class EquipTool : Equip
 {
-    // Start is called before the first frame update
-    void Start()
+    public float attackRate;
+    private bool attacking;
+    public float attackDistance;
+
+    [Header("Resource Gathering")]
+    public bool doesGatheringResources;
+
+    [Header("Combat")]
+    public bool doesDealDamage;
+    public int damage;
+
+    //components
+    private Animator anim;
+    private Camera cam;
+
+    void Awake()
     {
-        
+        //get our component
+        anim = GetComponent<Animator>();
+        cam = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnAttackInput()
     {
-        
+        if (!attacking) 
+        {
+            attacking = true;
+            anim.SetTrigger("Attack");
+            Invoke("OnCanAttack", attackRate);
+        }
+    }
+
+    void OnCanAttack()
+    {
+        attacking = true;
+    }
+
+    public void OnHit()
+    {
+
     }
 }
