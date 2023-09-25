c19ea3294af77cc2efb8ca10f261e1d7458631f2
Jedi-Daddy <av.zhuravlev90@gmail.com>
2023-09-25 16:43:10 +0200

    add crafting recipe ui script, add crafting window script

---
+++ SurvivalAcademy/Assets/Scripts/Crafting/CraftingRecipeUI.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CraftingRecipeUI : MonoBehaviour
+{
+    public CraftingRecipe recipe;
+    public Image backgroundImage;
+    public Image icon;
+    public TextMeshProUGUI itemName;
+    public Image[] resourceCosts;
+
+    public Color canCraftColor;
+    public Color cannotCraftColor;
+    private bool canCraft;
+
+    // updates the UI to display whether or not we can craft the recipe
+    public void UpdateCanCraft()
+    {
+        canCraft = true;
+        for (int i = 0; i < recipe.cost.Length; i++)
+        {
+            if (!Inventory.instance.HasItems(recipe.cost[i].item, recipe.cost[i].quantity))
+            {
+                canCraft = false;
+                break;
+            }
+        }
+        backgroundImage.color = canCraft ? canCraftColor : cannotCraftColor;
+    }
+}
