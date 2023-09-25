353bf6acdf1d410d7a1c8faa81a5955477feb988
Jedi-Daddy <av.zhuravlev90@gmail.com>
2023-09-25 17:13:41 +0200

    finish crafting window script and add crafting table

--- SurvivalAcademy/Assets/Scripts/Crafting/CraftingRecipeUI.cs
+++ SurvivalAcademy/Assets/Scripts/Crafting/CraftingRecipeUI.cs
@@ -16,6 +16,11 @@ public class CraftingRecipeUI : MonoBehaviour
     public Color cannotCraftColor;
     private bool canCraft;
 
+    void OnEnable()
+    {
+        UpdateCanCraft();
+    }
+
     // updates the UI to display whether or not we can craft the recipe
     public void UpdateCanCraft()
     {
@@ -29,5 +34,30 @@ public class CraftingRecipeUI : MonoBehaviour
             }
         }
         backgroundImage.color = canCraft ? canCraftColor : cannotCraftColor;
+    }
+    void Start()
+    {
+        icon.sprite = recipe.itemToCraft.icon;
+        itemName.text = recipe.itemToCraft.displayName;
+
+        for (int i = 0; i < resourceCosts.Length; i++)
+        {
+            if (i < recipe.cost.Length)
+            {
+                resourceCosts[i].gameObject.SetActive(true);
+                resourceCosts[i].sprite = recipe.cost[i].item.icon;
+                resourceCosts[i].transform.GetComponentInChildren<TextMeshProUGUI>().text = recipe.cost[i].quantity.ToString();
+            }
+            else
+                resourceCosts[i].gameObject.SetActive(false);
+        }
+    }
+
+    public void OnClickButton()
+    {
+        if (canCraft)
+        {
+            CraftingWindow.instance.Craft(recipe);
+        }
     }
 }
