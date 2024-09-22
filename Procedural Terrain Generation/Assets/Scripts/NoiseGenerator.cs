0736cab69648a3f4b719ac3cbd7a83355384c74a
Jedi-Daddy <av.zhuravlev90@gmail.com>
2024-09-22 18:32:20 +0200

    start create NoiseGenerator

---
+++ Procedural Terrain Generation/Assets/Scripts/NoiseGenerator.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseGenerator
+{
+    public static float[,] GenerateNoiseMap(int noiseSampleSize, float scale)
+    {
+        float[,] noiseMap = new float[noiseSampleSize, noiseSampleSize];
+        return noiseMap;
+    }
+}
